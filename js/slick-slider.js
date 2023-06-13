7a4d0d82124b887a0fdcef33cceff465b7db81c9
insusong <songinsu100@gmail.com>
2023-06-13 18:43:58 +0900

    header-working

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('.big_visual').slick({
-    dots: true,
     infinite: true,
     speed: 300,
     resizing: true,
@@ -9,31 +8,4 @@ $(document).ready(function(){
     autoplaySpeed: 6000,
     fade: true
   });
-  $('.play').click(function(){
-    $('.big_visual').slick('slickPlay');
-  });
-   
-  $('.stop').click(function(){
-    $('.big_visual').slick('slickPause');
-  });
-
-
-  // $('.business-visual').slick({
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   infinite: true,
-  //   speed: 300,
-  //   slidesToShow: 1,
-  //   centerMode: true,
-  //   variableWidth: true
-  // });
-
-  // $('.business-visual-mobile').slick({
-  //   autoplay: true,
-  //   autoplaySpeed: 3000,
-  //   infinite: true,
-  //   speed: 300,
-  //   slidesToShow: 1,
-  //   adaptiveHeight: true
-  // });
 });
